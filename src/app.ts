103096dec3ef3c8fd4933f71c11090462af93a59
Precell <precell@uncommon.org>
2024-02-23 09:33:47 +0200

    created autobind fn

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+// Autobind Decorator
+function autobind(_:any, _2: string, descriptor: PropertyDescriptor) {
+   const originalMethod = descriptor.value;
+   const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get(){
+      const boundFn = originalMethod.bind(this)
+      return boundFn
+    }
+   }
+
+   return adjDescriptor
+}
+// Project Input Class
 class ProjectInput {
   templateEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
@@ -29,8 +43,10 @@ class ProjectInput {
     console.log(this.titleElement.value);
     
   }
+  
+  @autobind
   private configure(){
-    this.element.addEventListener('submit', this.submitHandler.bind(this))
+    this.element.addEventListener('submit', this.submitHandler)
   }
   private attach() {
     this.hostEl.insertAdjacentElement("afterbegin", this.element);
